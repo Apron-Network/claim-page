95c7c03b304ea25add058f20e808b2377a6a28b9
srdothunter <srdothunter@gmail.com>
2021-04-20 10:57:23 +0800

    first commit

---
+++ precache-manifest.617251640fc6d9ddd9cb8e0837ba9013.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00180aa7b9ee38487f7d4f17da2d008e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7538919e27b900be8f89",
+    "url": "/static/css/main.0bf1af27.chunk.css"
+  },
+  {
+    "revision": "35704be8e44da412abb2",
+    "url": "/static/js/2.8df0f32c.chunk.js"
+  },
+  {
+    "revision": "a8dba0fdfed2bb7bc12e598c45e522d2",
+    "url": "/static/js/2.8df0f32c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7538919e27b900be8f89",
+    "url": "/static/js/main.28cdbdcc.chunk.js"
+  },
+  {
+    "revision": "4531f37e7b54b3c56c7e",
+    "url": "/static/js/runtime-main.d519985c.js"
+  },
+  {
+    "revision": "032381949ba2f893f198e1716dc73cc8",
+    "url": "/static/media/Monda-Bold.03238194.woff"
+  },
+  {
+    "revision": "6f1335bcd3a79cbd566726519199c34f",
+    "url": "/static/media/Monda-Bold.6f1335bc.ttf"
+  },
+  {
+    "revision": "ae359f55aa3b2e2373f4688c43524971",
+    "url": "/static/media/Monda-Bold.ae359f55.woff2"
+  },
+  {
+    "revision": "235a0b4d7906b2e29525e65d11847e0c",
+    "url": "/static/media/Monda-Regular.235a0b4d.woff2"
+  },
+  {
+    "revision": "7204684a10eec43c635120be84949c81",
+    "url": "/static/media/Monda-Regular.7204684a.woff"
+  },
+  {
+    "revision": "cc32ac407ade87819f32ce6f87883070",
+    "url": "/static/media/Monda-Regular.cc32ac40.ttf"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "9465c698c634285ca9f847d26dbef881",
+    "url": "/static/media/logo.9465c698.png"
+  }
+]);
